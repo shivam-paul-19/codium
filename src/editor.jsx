40571dc2f5ef9cdb1bed6ea52f56caf5e8e746ea
Shivam Paul <shivampaul2319@gmail.com>
2025-11-26 18:32:13 +0530

    side bar UI

--- src/editor.jsx
+++ src/editor.jsx
@@ -1,10 +1,42 @@
 import Editor from '@monaco-editor/react';
 import './editor.css';
+import Person from './person';
+import { toast } from "sonner"
 
 const CodeEditor = () => {
+  const handleCopyId = () => {
+    toast.success("Room ID copied!");
+  }
+
+  const ppl = [
+    {userId: 1, userName: "Shivam Paul"},
+    {userId: 2, userName: "Virat Kohli"},
+    {userId: 3, userName: "Rohit Sharma"},
+    {userId: 4, userName: "KL Rahul"},
+    {userId: 5, userName: "Sherlock Homes"},
+    {userId: 6, userName: "Tony Stark"}
+  ]
+  
   return (
     <>
     <div className="editor-page">
+      <div className="side-bar">
+        <div className="upper-side-bar">
+        <h1 className='side-bar-head'>In the room</h1>
+        <div className="ppl">
+          {
+            ppl.map((user, key) => {
+              let c = Math.floor(Math.random()*5);
+              return <Person username={user.userName} color={c}/>
+            })
+          }
+          </div>
+          </div>
+          <div className="editor-buttons">
+            <button className='copy-id-but' onClick={handleCopyId}>Copy ID</button><br />
+            <button className='leave-but'>Leave room</button>
+          </div>
+      </div>
     <div className="editor-container">
       <Editor
         height="90vh"
---
+++ src/person.jsx
@@ -0,0 +1,47 @@
+import {
+  Avatar,
+  AvatarFallback
+} from "@/components/ui/avatar";
+import "./person.css";
+
+function Person({username="User", color}) {
+    const extractInit = (name) => {
+        // make an array
+        let names = name.split(" ");
+        if(names.length == 1) {
+            return names[0].charAt(0).toUpperCase();
+        } else {
+            return names[0].charAt(0).toUpperCase() + names[1].charAt(0).toUpperCase();
+        }
+    }
+
+    const adjustName = (name) => {
+        if(name.length <= 14) return name;
+        // else adjust it 
+        return name.slice(0, 11) + "...";
+    }
+
+    let colors = [
+        {bg: "#fca5a5", letter: "#450a0a"},
+        {bg: "#fde047", letter: "#422006"},
+        {bg: "#6ee7b7", letter: "#022c22"},
+        {bg: "#67e8f9", letter: "#083344"},
+        {bg: "#d8b4fe", letter: "#3b0764"}
+    ]
+
+  return (
+    <>
+        <div className="person-box">
+            <Avatar>
+                <AvatarFallback style={{
+                    backgroundColor: colors[color].bg,
+                    color: colors[color].letter
+                }}>{extractInit(username)}</AvatarFallback>
+            </Avatar>
+            <h1 className="username">{adjustName(username)}</h1>
+        </div>
+    </>
+  )
+}
+
+export default Person
