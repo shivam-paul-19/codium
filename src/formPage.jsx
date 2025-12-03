59f80313c47d4cbfc9a2a3cdf5212e375e54f76c
Shivam Paul <shivampaul2319@gmail.com>
2025-12-03 20:42:20 +0530

    logo added

--- src/formPage.jsx
+++ src/formPage.jsx
@@ -1,9 +1,15 @@
 import Form from './Form'
-import './Form.css'
+import './Form.css';
+import logo from "./assests/codium_logo.png";
 
 const FormPage = () => {
   return (
     <div className="glass-container">
+      <div className="logo-container">
+      <img className='logo' src={logo} alt="" />
+      <p>Sync the code...</p>
+      </div>
+      <div className="separator"></div>
       <Form />
     </div>
   )
